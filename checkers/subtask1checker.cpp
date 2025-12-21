6409c8483ff2921386844913e9015311f0cb496f
Hubert <h.makowski66@gmail.com>
2025-12-21 20:31:30 +0100

    Added checker for subtask 1

---
+++ checkers/subtask1checker.cpp
@@ -0,0 +1,19 @@
+#include "testlib.h"
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+
+    registerTestlibCmd(argc, argv);
+
+    string userAnswer = ouf.readLine();
+
+    if (userAnswer == "Mare Boreum") quitp(_pc(2), "Poprawna odpowiedź.");
+    else if (userAnswer == "Sinus Sabaeus") quitp(_pc(1), "Podano nazwę obszaru, na którym znaleziono najmniej wody.");
+    else if (userAnswer == "MC-01") quitp(_pc(1), "Podano kod obszaru zamiast jego nazwy.");
+    else quitp(_pc(0), "Odpowiedź " + userAnswer + " nie jest poprawna");
+}
