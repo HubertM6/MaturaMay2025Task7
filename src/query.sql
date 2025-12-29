af337b37d5723d614d469dfd98337fbcb9a7bda4
Hubert <h.makowski66@gmail.com>
2025-12-29 14:59:47 +0100

    Updating

--- src/query.sql
+++ src/query.sql
@@ -1,7 +1,15 @@
--- task_1 BEGIN
+-- umieść swoje rozwiązania podzadań w odpowiedniej sekcji przed wysłaniem zgłoszenia
+-- aby uruchomić kwerendę i zobaczyć jej wynik umieść ją w sekcji "playground"
+-- pamiętaj że kwerenda w sekcji "playground" nie jest sprawdzana, więc przenieś ją do odpowiedniej sekcji podzadania przed wysłaniem zgłoszenia
+
+-- playground BEGIN
 SELECT * FROM laziki
+-- playground END
+
+-- task_1 BEGIN
+
 -- task_1 END
 
 -- task_2 BEGIN
-SELECT * FROM obszary
+
 -- task_2 END
